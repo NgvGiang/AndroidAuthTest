5ec8fd67e1d8d0e6b358fae69227cf4c8d9a72fb
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-09-16 09:10:10 +0700

    add fragment to Create, fragment to Read

--- src/main/java/vn/edu/usth/firebasesdkauthtest/MainActivity.java
+++ src/main/java/vn/edu/usth/firebasesdkauthtest/MainActivity.java
@@ -4,16 +4,26 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-    TextView greeting,randomNumber;
-
-
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        greeting = findViewById(R.id.greeting);
-        randomNumber = findViewById(R.id.random_number);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPagerAdapter.addFragment(new PersonalInfoFragment(), "Personal Info");
+        viewPagerAdapter.addFragment(new JobInfoFragment(), "Job Info");
+        viewPager.setAdapter(viewPagerAdapter);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            tab.setText(viewPagerAdapter.getPageTitle(position));
+        }).attach();
     }
 }
